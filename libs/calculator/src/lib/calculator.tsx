62043a38a764ae4301f65a0e03d57393dab84bd7
Ronald Pacheco <rpacheco124@gmail.com>
2024-11-19 22:24:58 +0100

    feat(calculator): generate empty calculator library

---
+++ libs/calculator/src/lib/calculator.tsx
@@ -0,0 +1,14 @@
+import styled from 'tailwind';
+
+const StyledCalculator = styled.div`
+  color: pink;
+`;
+export function Calculator() {
+  return (
+    <StyledCalculator>
+      <h1>Welcome to Calculator!</h1>
+    </StyledCalculator>
+  );
+}
+
+export default Calculator;
